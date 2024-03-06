99597dfb07cebd0195aa15880600fc6cc25d109a
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 18:26:35 +0100

    a

---
+++ src/main/java/Mantenimiento_Seguridad/Seguridad.java
@@ -0,0 +1,10 @@
+package Mantenimiento_Seguridad;
+
+public class Seguridad extends Instalaciones{
+
+    // Atributos
+    String tipo_seguridad;
+    String fecha_activacion;
+    String hora_activacion;
+
+}
