372040244acdd71a3881889a9b0130c307e069df
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 18:05:05 +0100

    a

--- src/main/java/Mantenimiento_Seguridad/Mantenimiento.java
+++ src/main/java/Mantenimiento_Seguridad/Mantenimiento.java
@@ -7,4 +7,11 @@ public class Mantenimiento {
     String tipo;
     boolean mantenimientoProgramado;
 
+    // Constructor
+    public Mantenimiento(String nombre, String tipo, boolean mantenimientoProgramado) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.mantenimientoProgramado = mantenimientoProgramado;
+    }
+
 }
