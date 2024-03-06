7328eff25dee1e6951b2e53c09156668a803b06c
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 18:19:43 +0100

    a

--- src/main/java/Mantenimiento_Seguridad/Instalaciones.java
+++ src/main/java/Mantenimiento_Seguridad/Instalaciones.java
@@ -14,44 +14,5 @@ public class Instalaciones {
         this.tipo = tipo;
     }
 
-    // Métodos para realizar mantenimiento y reparaciones
-    public void realizarMantenimiento() {
-        System.out.println("Realizando mantenimiento en " + nombre);
-    }
-
-    public void realizarReparaciones() {
-        System.out.println("Realizando reparaciones en " + nombre);
-    }
-
-    // Getters y Setters
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getUbicacion() {
-        return ubicacion;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setUbicacion(String ubicacion) {
-        this.ubicacion = ubicacion;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    // Método toString
-    @Override
-    public String toString() {
-        return "Instalaciones{" + "nombre=" + nombre + ", ubicacion=" + ubicacion + ", tipo=" + tipo + '}';
-    }
 
 }
