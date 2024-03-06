d3286302b5f3ea70bb4762913e09a8b3d6c4452f
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 10:43:12 +0100

    a

---
+++ src/main/java/Hábitats/Habitat_Terrestre.java
@@ -0,0 +1,11 @@
+package Hábitats;
+
+public class Habitat_Terrestre extends Hábitat {
+    // Atributos clase Habitat_Terrestre
+    String tipo_suelo;
+    String vegetacion;
+    String rocas;
+    
+
+
+}
