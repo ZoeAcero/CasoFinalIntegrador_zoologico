df12f6e08babe6b72af0510b7b15f50f00d08ed0
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 11:41:33 +0100

    a

--- src/main/java/Animales/Animales_Aviarios.java
+++ src/main/java/Animales/Animales_Aviarios.java
@@ -7,5 +7,15 @@ public class Animales_Aviarios extends Animales{
     String pico;
     String tipo_patas;
 
-    
+// Constructor clase Animales_Aviarios
+    public Animales_Aviarios(String nombre, String tipo, String raza, String color, String edad, Float peso, Float altura, Boolean vacunado, Boolean esterilizado, String plumaje, String pico, String tipo_patas) {
+        super(nombre, tipo, raza, color, edad, peso, altura, vacunado, esterilizado);
+        this.plumaje = plumaje;
+        this.pico = pico;
+        this.tipo_patas = tipo_patas;
+    }
+
+   
+
+
 }
