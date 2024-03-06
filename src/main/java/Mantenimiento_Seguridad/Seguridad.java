11e14fb7eeb5380c89a91c21d16cb24e6a17ea77
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 18:32:18 +0100

    a

--- src/main/java/Mantenimiento_Seguridad/Seguridad.java
+++ src/main/java/Mantenimiento_Seguridad/Seguridad.java
@@ -1,15 +1,15 @@
 package Mantenimiento_Seguridad;
 
-public class Seguridad extends Instalaciones{
+public class Seguridad{
 
     // Atributos
     String tipo_seguridad;
     String fecha_activacion;
     String hora_activacion;
+    String nombre;
 
     // Constructor
     public Seguridad(String nombre, String ubicacion, String tipo, String tipo_seguridad, String fecha_activacion, String hora_activacion) {
-        super(nombre, ubicacion, tipo);
         this.tipo_seguridad = tipo_seguridad;
         this.fecha_activacion = fecha_activacion;
         this.hora_activacion = hora_activacion;
@@ -18,10 +18,6 @@ public class Seguridad extends Instalaciones{
     // Métodos para activar y desactivar seguridad
     public void activarSeguridad() {
         System.out.println("Activando seguridad en " + nombre);
-    }
-
-    public void desactivarSeguridad() {
-        System.out.println("Desactivando seguridad en " + nombre);
     }
 
     // Getters y Setters
