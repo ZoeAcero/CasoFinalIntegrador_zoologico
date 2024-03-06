367b850a4bae744c55d0bcbf4b056630eac82b33
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 10:36:29 +0100

    a

--- src/main/java/Hábitats/Habitat_Acuatico.java
+++ src/main/java/Hábitats/Habitat_Acuatico.java
@@ -1,11 +1,20 @@
 package Hábitats;
 
-public class Habitat_Acuatico extends Hábitats {
+public class Habitat_Acuatico extends Hábitat {
     // Atributos clase Habitat_Acuatico
     String tipo_agua;
 String salinidad;
 String corales;
 
+// Constructor clase Habitat_Acuatico
+public Habitat_Acuatico(String nombre, String humedad, String temperatura, String tipo, String tamaño, String limpieza, String tipo_agua, String salinidad, String corales) {
+    super(nombre, humedad, temperatura, tipo, tamaño, limpieza);
+}
+
+// Getters y Setters
+public String getTipo_agua() {
+    return tipo_agua;
+    
 
 
 
