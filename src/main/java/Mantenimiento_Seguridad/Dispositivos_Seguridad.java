7503d5dfaa366a36e01f8a6ca0cbe7535a0eb860
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 17:47:17 +0100

    a

--- src/main/java/Mantenimiento_Seguridad/Dispositivos_Seguridad.java
+++ src/main/java/Mantenimiento_Seguridad/Dispositivos_Seguridad.java
@@ -2,5 +2,11 @@ package Mantenimiento_Seguridad;
 
 public class Dispositivos_Seguridad extends Seguridad{
 
-    
+    public abstract class DispositivoSeguridad {
+        protected int id;
+
+        // Constructor
+        public DispositivoSeguridad(int id) {
+            this.id = id;
+        }
 }
