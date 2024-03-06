8dabfc3960a540a197817ee6f167f6fe74678782
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 09:56:39 +0100

    a

---
+++ src/main/java/Animales/Animales.java
@@ -0,0 +1,14 @@
+package Animales;
+
+public class Animales {
+
+    String nombre;
+    String tipo;
+    String raza;
+    String color;
+    String edad;
+    Float peso;
+    Float altura;
+}
+
+
