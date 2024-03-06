a5564b440ba52ad56f42ea796a1b5d9858415327
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 18:26:51 +0100

    a

--- src/main/java/Mantenimiento_Seguridad/Seguridad.java
+++ src/main/java/Mantenimiento_Seguridad/Seguridad.java
@@ -7,4 +7,12 @@ public class Seguridad extends Instalaciones{
     String fecha_activacion;
     String hora_activacion;
 
+    // Constructor
+    public Seguridad(String nombre, String ubicacion, String tipo, String tipo_seguridad, String fecha_activacion, String hora_activacion) {
+        super(nombre, ubicacion, tipo);
+        this.tipo_seguridad = tipo_seguridad;
+        this.fecha_activacion = fecha_activacion;
+        this.hora_activacion = hora_activacion;
+    }
+
 }
