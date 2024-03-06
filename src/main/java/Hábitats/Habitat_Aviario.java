df21f88cfb3d790cdff5cc8eb49f071dcdda4108
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 10:49:32 +0100

    a

---
+++ src/main/java/Hábitats/Habitat_Aviario.java
@@ -0,0 +1,11 @@
+package Hábitats;
+
+public class Habitat_Aviario extends Hábitat{
+
+    // Atributos clase Habitat_Aviario
+    String tipo_aves;
+    String tipo_alimentacion;
+    String tipo_nidos;
+
+  
+}
