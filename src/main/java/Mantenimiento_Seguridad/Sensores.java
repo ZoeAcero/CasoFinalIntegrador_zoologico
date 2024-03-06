416953dda6f436a9f6677d21c56227439242434f
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 18:38:05 +0100

    a

---
+++ src/main/java/Mantenimiento_Seguridad/Sensores.java
@@ -0,0 +1,12 @@
+package Mantenimiento_Seguridad;
+
+public class Sensores extends Seguridad{
+
+    // Atributos
+    String tipo_sensor;
+    String ubicacion_sensor;
+    String fecha_instalacion;
+
+    // Constructor
+    
+}
