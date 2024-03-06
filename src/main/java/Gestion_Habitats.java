9fe5d2942614388cf0ff7d71050801bdae3ae901
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 09:47:15 +0100

    a

--- src/main/java/Gestion_Habitats.java
+++ src/main/java/Gestion_Habitats.java
@@ -12,3 +12,10 @@ class Habitat {
     public void monitorear() {
         System.out.println("Monitoreando el hábitat " + nombre + "...");
     }
+    public void mostrarCondiciones() {
+        System.out.println("Condiciones del hábitat " + nombre + ":");
+        System.out.println("Temperatura: " + temperatura + "°C");
+        System.out.println("Humedad: " + humedad + "%");
+        System.out.println("Limpieza: " + limpieza + "%\n");
+    }
+}
