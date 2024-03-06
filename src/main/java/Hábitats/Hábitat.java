7cf635bf6bcaabf9319972c9e148ccba243a5a5c
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 10:13:10 +0100

    a

---
+++ src/main/java/Hábitats/Hábitat.java
@@ -0,0 +1,12 @@
+package Hábitats;
+
+public class Hábitat {
+
+    // Atributos clase Hábitat
+    String nombre;
+    String humedad;
+    String temperatura;
+    String tipo;
+    String tamaño;
+    
+}
