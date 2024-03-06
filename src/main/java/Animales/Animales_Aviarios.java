a0083e5acd2b413c2ffc6e618f0c26d3b4e2c478
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 11:40:57 +0100

    a

--- src/main/java/Animales/Animales_Aviarios.java
+++ src/main/java/Animales/Animales_Aviarios.java
@@ -2,5 +2,10 @@ package Animales;
 
 public class Animales_Aviarios extends Animales{
 
+    // Atributos clase Animales_Aviarios
+    String plumaje;
+    String pico;
+    String tipo_patas;
+
     
 }
