751359319c3674d69908ad919fc2ed2a292301ec
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 18:18:25 +0100

    a

--- src/main/java/Mantenimiento_Seguridad/Mantenimiento.java
+++ src/main/java/Mantenimiento_Seguridad/Mantenimiento.java
@@ -8,5 +8,11 @@ public class Mantenimiento extends Instalaciones{
     String hora_mantenimiento;
 
     // Constructor
-    
+    public Mantenimiento(String nombre, String ubicacion, String tipo, String tipo_mantenimiento, String fecha_mantenimiento, String hora_mantenimiento) {
+        super(nombre, ubicacion, tipo);
+        this.tipo_mantenimiento = tipo_mantenimiento;
+        this.fecha_mantenimiento = fecha_mantenimiento;
+        this.hora_mantenimiento = hora_mantenimiento;
+    }
+
 }
