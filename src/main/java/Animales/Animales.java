f01ffb7e02c84940cba9d46f0354d8f0fadd1804
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 09:59:48 +0100

    a

--- src/main/java/Animales/Animales.java
+++ src/main/java/Animales/Animales.java
@@ -9,6 +9,16 @@ public class Animales {
     String edad;
     Float peso;
     Float altura;
+
+    public Animales(String nombre, String tipo, String raza, String color, String edad, Float peso, Float altura) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.raza = raza;
+        this.color = color;
+        this.edad = edad;
+        this.peso = peso;
+        this.altura = altura;
+    }
 }
 
 
