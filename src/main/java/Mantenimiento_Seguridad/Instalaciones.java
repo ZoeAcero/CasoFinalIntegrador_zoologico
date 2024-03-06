11c2b65f2b845a1058b8022a539a9bd2ee6a9a90
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 17:59:36 +0100

    a

--- src/main/java/Mantenimiento_Seguridad/Instalaciones.java
+++ src/main/java/Mantenimiento_Seguridad/Instalaciones.java
@@ -8,5 +8,10 @@ public class Instalaciones {
     String tipo;
 
     // Constructor
-    
+    public Instalaciones(String nombre, String ubicacion, String tipo) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.tipo = tipo;
+    }
+
 }
