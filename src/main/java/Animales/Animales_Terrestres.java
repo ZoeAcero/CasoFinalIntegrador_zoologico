5fe8145762d84be1b0b45fc1afb5c2cc54b4c654
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 11:06:40 +0100

    a

---
+++ src/main/java/Animales/Animales_Terrestres.java
@@ -0,0 +1,11 @@
+package Animales;
+
+public class Animales_Terrestres extends Animales{
+
+    // Atributos clase Animales_Terrestres
+    String pelaje;
+    String garras;
+    String tipo_patas;
+
+   
+}
