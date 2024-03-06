47b6441976a1d961f0a219919a29e8fa61fc03aa
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 18:34:02 +0100

    a

--- src/main/java/Mantenimiento_Seguridad/Camara.java
+++ src/main/java/Mantenimiento_Seguridad/Camara.java
@@ -8,7 +8,13 @@ public class Camara extends Seguridad {
     String fecha_instalacion;
 
     // Constructor
-    
+    public Camara(String nombre, String ubicacion, String tipo, String tipo_seguridad, String fecha_activacion, String hora_activacion, String tipo_camara, String ubicacion_camara, String fecha_instalacion) {
+        super(nombre, ubicacion, tipo, tipo_seguridad, fecha_activacion, hora_activacion);
+        this.tipo_camara = tipo_camara;
+        this.ubicacion_camara = ubicacion_camara;
+        this.fecha_instalacion = fecha_instalacion;
+    }
+
 
 
 }
