acdf37fde3595bdca614d2ddf81e90d07b09b1a6
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 17:44:47 +0100

    a

--- src/main/java/Mantenimiento_Seguridad/Seguridad.java
+++ src/main/java/Mantenimiento_Seguridad/Seguridad.java
@@ -2,5 +2,13 @@ package Mantenimiento_Seguridad;
 
 public class Seguridad {
 
+    private List<Camara> camaras;
+    private List<Sensor> sensores;
+
+    // Constructor
+    public Seguridad(List<Camara> camaras, List<Sensor> sensores) {
+        this.camaras = camaras;
+        this.sensores = sensores;
+    }
 
 }
