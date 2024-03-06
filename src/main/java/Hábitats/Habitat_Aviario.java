74db4ecddfc0fb5d2ea4d5d58d720393bc823ed7
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 10:51:36 +0100

    a

--- src/main/java/Hábitats/Habitat_Aviario.java
+++ src/main/java/Hábitats/Habitat_Aviario.java
@@ -7,5 +7,11 @@ public class Habitat_Aviario extends Hábitat{
     String tipo_alimentacion;
     String tipo_nidos;
 
-  
+    // Constructor clase Habitat_Aviario
+    public Habitat_Aviario(String nombre, String humedad, String temperatura, String tipo, String tamaño, String limpieza, String tipo_aves, String tipo_alimentacion, String tipo_nidos) {
+        super(nombre, humedad, temperatura, tipo, tamaño, limpieza);
+        this.tipo_aves = tipo_aves;
+        this.tipo_alimentacion = tipo_alimentacion;
+        this.tipo_nidos = tipo_nidos;
+    }
 }
