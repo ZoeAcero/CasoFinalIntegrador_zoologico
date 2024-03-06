18c546ccf4388d02d6e69c58d625f41a2e69d48a
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 18:38:21 +0100

    a

--- src/main/java/Mantenimiento_Seguridad/Sensores.java
+++ src/main/java/Mantenimiento_Seguridad/Sensores.java
@@ -8,5 +8,11 @@ public class Sensores extends Seguridad{
     String fecha_instalacion;
 
     // Constructor
-    
+    public Sensores(String nombre, String ubicacion, String tipo, String tipo_seguridad, String fecha_activacion, String hora_activacion, String tipo_sensor, String ubicacion_sensor, String fecha_instalacion) {
+        super(nombre, ubicacion, tipo, tipo_seguridad, fecha_activacion, hora_activacion);
+        this.tipo_sensor = tipo_sensor;
+        this.ubicacion_sensor = ubicacion_sensor;
+        this.fecha_instalacion = fecha_instalacion;
+    }
+
 }
