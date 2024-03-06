7ce754ebafeddf8daa00c98dd0ef07d8acdc1861
ZoeAcero <zar.amazona@gmail.com>
2024-03-06 10:14:25 +0100

    a

--- src/main/java/Hábitats/Hábitat.java
+++ src/main/java/Hábitats/Hábitat.java
@@ -9,4 +9,14 @@ public class Hábitat {
     String tipo;
     String tamaño;
     String limpieza;
+
+    // Constructor clase Hábitat
+    public Hábitat(String nombre, String humedad, String temperatura, String tipo, String tamaño, String limpieza) {
+        this.nombre = nombre;
+        this.humedad = humedad;
+        this.temperatura = temperatura;
+        this.tipo = tipo;
+        this.tamaño = tamaño;
+        this.limpieza = limpieza;
+    }
 }
