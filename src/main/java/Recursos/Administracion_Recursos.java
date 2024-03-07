cc04e94f1a408b08a339e190a9b8400f94c768fc
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 11:53:29 +0100

    a

--- src/main/java/Recursos/Administracion_Recursos.java
+++ src/main/java/Recursos/Administracion_Recursos.java
@@ -3,6 +3,11 @@ package Recursos;
 public class Administracion_Recursos {
 
     // Atributos de Administracion de Recursos
+    String nombre;
+    double precio;
+    String ubicacion;
+
+    // Constructor
     
 
 }
