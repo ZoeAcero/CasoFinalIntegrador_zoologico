0249fdb3dd9f7460b20c4343bab7da358b044c02
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 12:17:32 +0100

    a

---
+++ src/main/java/Recursos/Medicinas.java
@@ -0,0 +1,12 @@
+package Recursos;
+
+public class Medicinas extends Administracion_Recursos{
+
+    // Atributos
+    String tipo_medicina;
+    String fecha_caducidad;
+    Float precio;
+
+    // Constructor
+    
+}
