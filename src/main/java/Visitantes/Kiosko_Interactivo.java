fad74deb02483d8ac58e0afd8f6b01bd2985b208
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 19:18:12 +0100

    a

--- src/main/java/Visitantes/Kiosko_Interactivo.java
+++ src/main/java/Visitantes/Kiosko_Interactivo.java
@@ -3,7 +3,17 @@ import Animales.Animales;
 public class Kiosko_Interactivo {
 public void interactuarConAnimales(Animales animal) {
 
-
+    System.out.println("Informacion del Animal: ");
+    System.out.println("Nombre: " + animal.getNombre());
+    System.out.println("tipo: " + animal.getTipo());
+    System.out.println("Edad: " + animal.getEdad());
+    System.out.println("raza: " + animal.getRaza());
+    System.out.println("Peso: " + animal.getPeso());
+    System.out.println("Altura: " + animal.getAltura());
+    System.out.println("Color: " + animal.getColor());
+    System.out.println("Vacunado: " + animal.getVacunado());
+    System.out.println("Esterilizado: " + animal.getEsterilizado());
 }
+
 
 }
