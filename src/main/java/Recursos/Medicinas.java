64b0f100b76e8dd1359158f62fb2add71bcf841a
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 12:17:50 +0100

    a

--- src/main/java/Recursos/Medicinas.java
+++ src/main/java/Recursos/Medicinas.java
@@ -8,5 +8,9 @@ public class Medicinas extends Administracion_Recursos{
     Float precio;
 
     // Constructor
-    
+    public Medicinas(String nombre, double precio, String ubicacion, String tipo_medicina, String fecha_caducidad) {
+        super(nombre, precio, ubicacion);
+        this.tipo_medicina = tipo_medicina;
+        this.fecha_caducidad = fecha_caducidad;
+    }
 }
