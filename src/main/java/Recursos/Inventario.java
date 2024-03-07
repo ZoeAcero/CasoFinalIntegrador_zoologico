c4601614d73817b55c1dc4919fd4e6ef4bdbfc8c
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 18:26:24 +0100

    a

--- src/main/java/Recursos/Inventario.java
+++ src/main/java/Recursos/Inventario.java
@@ -16,6 +16,10 @@ public class Inventario {
         public void actualizarInventario(Recurso recurso, int cantidad) {
             // Actualizar la cantidad en el inventario
         }
+        public void mostrarInventario() {
+            // Mostrar el inventario actual
+        }
+    }
 
 
 
