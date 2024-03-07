32f05f2a09e754f92fa58d27c00b8bed0187d80f
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 19:36:32 +0100

    a

--- src/main/java/Recursos/Alimentos.java
+++ src/main/java/Recursos/Alimentos.java
@@ -9,31 +9,6 @@ public class Alimentos {
     String proveedor;
 
     // Constructor
-    public Alimentos(String nombre, double precio, String ubicacion, String tipo_alimento, Boolean perecedero) {
-        this.tipo_alimento = tipo_alimento;
-    }
-
-    // Getters y Setters
-    public String getTipo_alimento() {
-        return tipo_alimento;
-    }
-
-    public Boolean getPerecedero() {
-        return perecedero;
-    }
-
-    public void setTipo_alimento(String tipo_alimento) {
-        this.tipo_alimento = tipo_alimento;
-    }
-
-    public void setPerecedero(Boolean perecedero) {
-        this.perecedero = perecedero;
-    }
-
-    // Método toString
-    @Override
-    public String toString() {
-        return "Alimentos{" + "tipo_alimento=" + tipo_alimento + ", perecedero=" + perecedero + '}';
-    }
+   
 
 }
