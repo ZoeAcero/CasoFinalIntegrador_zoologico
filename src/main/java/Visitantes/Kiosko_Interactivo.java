be2996ecac800191e940bf3ee3d4cf0e379a39fe
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 19:04:36 +0100

    a

---
+++ src/main/java/Visitantes/Kiosko_Interactivo.java
@@ -0,0 +1,7 @@
+package Visitantes;
+
+public class Kiosko_Interactivo {
+    public void realizarTour() {
+        System.out.println(("¡Bienvenidos! Vamos a explorar el kiosko interactivo y aprender sobre los animales."));
+    }
+}
