9b69c435c620572f552247780d0adbedef4842bc
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 12:01:47 +0100

    a

--- src/main/java/Recursos/Alimentos.java
+++ src/main/java/Recursos/Alimentos.java
@@ -6,5 +6,9 @@ public class Alimentos extends Administracion_Recursos{
     String tipo_alimento;
 
     // Constructor
-    
+    public Alimentos(String nombre, double precio, String ubicacion, String tipo_alimento) {
+        super(nombre, precio, ubicacion);
+        this.tipo_alimento = tipo_alimento;
+    }
+
 }
