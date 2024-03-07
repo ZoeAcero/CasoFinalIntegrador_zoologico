bc8acad72ab8d53b855477d91398c45dc9f6f704
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 18:25:29 +0100

    a

--- src/main/java/Recursos/Inventario.java
+++ src/main/java/Recursos/Inventario.java
@@ -1,5 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
 package Recursos;
 
 public class Inventario {
+    public class Inventario {
+        private Map<Recurso, Integer> inventario;
+
+        public Inventario() {
+            this.inventario = new HashMap<>();
+        }
+
     
 }
