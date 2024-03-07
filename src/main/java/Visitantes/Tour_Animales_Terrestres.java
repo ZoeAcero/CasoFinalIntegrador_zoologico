4057fe2724b46f4b3d273b1adbd06b66d3370637
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 18:57:37 +0100

    a

---
+++ src/main/java/Visitantes/Tour_Animales_Terrestres.java
@@ -0,0 +1,48 @@
+package Visitantes;
+
+public class Tour_Animales_Terrestres extends Visitantes implements TourGuiado{
+
+    // Constructor
+    public Tour_Animales_Terrestres(String Edad, String InteresVisitante, String fechaVisita, String horaVisita) {
+        super(Edad, InteresVisitante, fechaVisita, horaVisita);
+    }
+
+    // Getters y Setters
+    public String getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(String Edad) {
+        this.Edad = Edad;
+    }
+
+    public String getInteresVisitante() {
+        return InteresVisitante;
+    }
+
+    public void setInteresVisitante(String InteresVisitante) {
+        this.InteresVisitante = InteresVisitante;
+    }
+
+    public String getFechaVisita() {
+        return fechaVisita;
+    }
+
+    public void setFechaVisita(String fechaVisita) {
+        this.fechaVisita = fechaVisita;
+    }
+
+    public String getHoraVisita() {
+        return horaVisita;
+    }
+
+    public void setHoraVisita(String horaVisita) {
+        this.horaVisita = horaVisita;
+    }
+
+    // Metodo toString
+    @Override
+    public void realizarTour() {
+        System.out.println(("¡Bienvenidos! Vamos a explorar el área de animales terrestres y aprender sobre ellos."));
+    }
+}
