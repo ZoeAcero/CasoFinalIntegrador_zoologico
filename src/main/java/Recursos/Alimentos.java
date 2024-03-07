17af7a4467f981fa76a69bd2830aa148e0ce489c
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 12:03:08 +0100

    a

--- src/main/java/Recursos/Alimentos.java
+++ src/main/java/Recursos/Alimentos.java
@@ -4,9 +4,10 @@ public class Alimentos extends Administracion_Recursos{
 
     // Atributos
     String tipo_alimento;
+    Boolean perecedero;
 
     // Constructor
-    public Alimentos(String nombre, double precio, String ubicacion, String tipo_alimento) {
+    public Alimentos(String nombre, double precio, String ubicacion, String tipo_alimento, Boolean perecedero) {
         super(nombre, precio, ubicacion);
         this.tipo_alimento = tipo_alimento;
     }
@@ -14,6 +15,10 @@ public class Alimentos extends Administracion_Recursos{
     // Getters y Setters
     public String getTipo_alimento() {
         return tipo_alimento;
+    }
+
+    public Boolean getPerecedero() {
+        return perecedero;
     }
 
 }
