82e2c50dcbac5e43e310474185a4c75b8c454da9
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 13:08:19 +0100

    a

--- src/main/java/Recursos/Inventario.java
+++ src/main/java/Recursos/Inventario.java
@@ -1,25 +1,5 @@
 package Recursos;
 
 public class Inventario {
-
-    private Map<Recurso, Integer> inventario;
-
-    public Inventario() {
-        this.inventario = new HashMap<>();
-    }
-
-    public void agregarRecurso(Recurso recurso, int cantidad) {
-        inventario.put(recurso, cantidad);
-    }
-
-    public void actualizarInventario(Recurso recurso, int cantidad) {
-        // Actualizar la cantidad en el inventario
-    }
-
-    public void mostrarInventario() {
-        // Mostrar el inventario actual
-    }
-}
-
-
+    
 }
