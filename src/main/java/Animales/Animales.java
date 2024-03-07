317fc7cc7ff29797f2b7eb1c9a9b2bcb6141b5b4
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 19:31:15 +0100

    a

--- src/main/java/Animales/Animales.java
+++ src/main/java/Animales/Animales.java
@@ -98,7 +98,7 @@ public class Animales {
         this.esterilizado = esterilizado;
     }
 
-    
+
 }
 
 
--- src/main/java/Visitantes/Kiosko_Interactivo.java
+++ src/main/java/Visitantes/Kiosko_Interactivo.java
@@ -1,5 +1,7 @@
 package Visitantes;
 import Animales.Animales;
+import Hábitats.Hábitat;
+
 public class Kiosko_Interactivo {
 public void interactuarConAnimales(Animales animal) {
 
@@ -13,7 +15,20 @@ public void interactuarConAnimales(Animales animal) {
     System.out.println("Color: " + animal.getColor());
     System.out.println("Vacunado: " + animal.getVacunado());
     System.out.println("Esterilizado: " + animal.getEsterilizado());
-}
+   }
+
+    public void interactuarConHabitat(Hábitat habitat) {
+     System.out.println("Informacion del Habitat: ");
+     System.out.println("Nombre: " + habitat.getNombre());
+     System.out.println("tipo: " + habitat.getTipo());
+     System.out.println("Tamaño: " + habitat.getTamaño());
+     System.out.println("Humedad: " + habitat.getHumedad());
+     System.out.println("Temperatura: " + habitat.getTemperatura());
+     System.out.println("Limpieza: " + habitat.getLimpieza());
+    }
+
+
+
 
 
 }
