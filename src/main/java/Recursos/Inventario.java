f3aaa6a0ad558019d0b05da3b8e45b7fb0d06eb0
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 19:43:54 +0100

    a

--- src/main/java/Recursos/Inventario.java
+++ src/main/java/Recursos/Inventario.java
@@ -6,5 +6,10 @@ public class Inventario {
     private List<Medicinas> medicinas;
     private List<Alimentos> alimentos;
     private List<Equipamiento> equipamiento;
-    
+
+    public Inventario() {
+        this.medicinas = new ArrayList<>();
+        this.alimentos = new ArrayList<>();
+        this.equipamiento = new ArrayList<>();
+    }
 }
