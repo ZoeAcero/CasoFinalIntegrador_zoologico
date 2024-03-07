de717b7e3c9a65f2932bb5d44dfad377216ae288
ZoeAcero <zar.amazona@gmail.com>
2024-03-07 18:30:17 +0100

    a

--- src/main/java/Recursos/Inventario.java
+++ src/main/java/Recursos/Inventario.java
@@ -1,26 +1,7 @@
-import java.util.HashMap;
-import java.util.Map;
+
 package Recursos;
 
 public class Inventario {
-    public class Inventario {
-        private Map<Recurso, Integer> inventario;
 
-        public Inventario() {
-            this.inventario = new HashMap<>();
-        }
-        public void agregarRecurso(Recurso recurso, int cantidad) {
-            inventario.put(recurso, cantidad);
-        }
-
-        public void actualizarInventario(Recurso recurso, int cantidad) {
-            // Actualizar la cantidad en el inventario
-        }
-        public void mostrarInventario() {
-            // Mostrar el inventario actual
-        }
-    }
-
-
-
-    }
+    
+}
