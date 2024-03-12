e9b47d587930e49b38501142ac55cf8027e058e3
ZoeAcero <zar.amazona@gmail.com>
2024-03-12 18:19:44 +0100

    a

--- src/main/java/Zoologico.java
+++ src/main/java/Zoologico.java
@@ -122,6 +122,19 @@ public class Zoologico {
                     }
                 }
 
+else if (opcion_visitante == 3) {
+                    System.out.println("Por favor ingrese el nombre del habitat que desea ver:");
+                    String nombre_habitat = scanner.nextLine();
+                    for (Hábitat habitat : habitats) {
+                        if (habitat.getNombre().equals(nombre_habitat)) {
+                            System.out.println(habitat);
+                        }
+                    }
+
+
+
+
+
 
 
             }
