c377481fe95cf54d1c9df4b356e5e6158efe029c
ZoeAcero <zar.amazona@gmail.com>
2024-03-12 19:40:18 +0100

    a

--- src/main/java/Zoologico.java
+++ src/main/java/Zoologico.java
@@ -33,8 +33,8 @@ public class Zoologico {
             if (opcion == 1 ) {
                 System.out.println("Bienvenido al Zoologico, por favor seleccione una opcion:");
                 System.out.println("1. Ver un animal");
-                System.out.println("2. Ver hábitat y cuidados de los animales");
-                System.out.println("3. Registrar un nuevo animal");
+                System.out.println("2. Registrar un nuevo animal");
+                System.out.println("3. Ver hábitat y cuidados de los animales");
                 System.out.println("4. Ir al kiosko interactivo");
                 System.out.println("5. Salir al menu principal");
 
